6d669fbbb7906e98f46fdef0efd007b9e88b0e88
captain-adi <maniramp554@gmail.com>
2026-01-21 11:49:18 +0530

    feat: enhance Footer component with theme-based text color and improved layout

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,14 +1,22 @@
-
+import { useTheme } from "@/context/ThemeProvider";
 
 function Footer() {
+  const { theme } = useTheme();
   return (
-    <footer className=" text-muted-foreground text-sm py-12 ">
-  <div className="max-w-7xl mx-auto px-4 flex flex-col md:flex-row items-center justify-between text-center md:text-left">
-    <p>© Copyright 2025. All rights Reserved.</p>
-    <p>Made by <span className="font-semibold text-white">Aditya Pandey</span></p>
-  </div>
-</footer>
-  )
+    <footer className=" text-muted-foreground text-sm py-12 container mx-auto">
+      <div className="max-w-7xl mx-auto px-4 flex flex-col md:flex-row items-center justify-between text-center md:text-left">
+        <p>© Copyright 2025. All rights Reserved.</p>
+        <p>
+          Made by{" "}
+          <span
+            className={`font-semibold ${theme === "dark" ? "text-white" : "text-black"}`}
+          >
+            Aditya Pandey
+          </span>
+        </p>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
